663805e74a73ed1ea9b2c5b70a78d420d02e2cf2
Isilud <jolan.houot@gmail.com>
2023-07-18 19:24:00 +0200

    completed todos tests & added discount test

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,12 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -13,7 +16,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.mockito.Mockito.when;
+
+import java.util.Date;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -25,6 +31,17 @@ public class ParkingDataBaseIT {
 
     @Mock
     private static InputReaderUtil inputReaderUtil;
+
+    private static Ticket createTicketWithOneHourInTimeOffset(ParkingService parkingService) {
+        Ticket ticket = new Ticket();
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - (  60 * 60 * 1000) );
+        ticket.setInTime(inTime);
+        ticket.setParkingSpot(parkingService.getNextParkingNumberIfAvailable());
+        ticket.setVehicleRegNumber("ABCDEF");
+        ticket.setPrice(0);
+        return ticket;
+    }
 
     @BeforeAll
     private static void setUp() throws Exception{
@@ -51,15 +68,31 @@ public class ParkingDataBaseIT {
     public void testParkingACar(){
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        Ticket registeredTicket = ticketDAO.getTicket("ABCDEF");
+        assert(registeredTicket != null );
+        assert(parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR) > 1 ); // Next empty place should be 2
     }
 
     @Test
     public void testParkingLotExit(){
-        testParkingACar();
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);            
+        Ticket ticket = createTicketWithOneHourInTimeOffset(parkingService);
+        ticketDAO.saveTicket(ticket);
         parkingService.processExitingVehicle();
-        //TODO: check that the fare generated and out time are populated correctly in the database
+        Ticket registeredTicket = ticketDAO.getTicket("ABCDEF");
+        assert(registeredTicket.getOutTime() != null);
+        assert(registeredTicket.getPrice() != 0);
     }
 
+    @Test
+    public void testParkingLotExitRecurringUser(){
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        Ticket ticket = createTicketWithOneHourInTimeOffset(parkingService);
+        ticketDAO.saveTicket(ticket);
+        parkingService.processExitingVehicle();     
+        ticketDAO.saveTicket(ticket);
+        parkingService.processExitingVehicle();
+        Ticket registeredTicket = ticketDAO.getTicket("ABCDEF");
+        assert(Math.abs(registeredTicket.getPrice() - Fare.CAR_RATE_PER_HOUR * 0.95) < 0.001);
+    }
 }
